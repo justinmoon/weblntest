cca04b5f6cca4a3cdacddbf92eaa94225b92823c
Justin Moon <mail@justinmoon.com>
2022-11-04 12:26:07 -0300

    Move methods

--- index.js
+++ index.js
@@ -18,3 +18,38 @@ function showResult(result) {
   resultElement.innerText = JSON.stringify(result, null, 4);
   resultElement.style.display = "block";
 }
+
+async function makeInvoice() {
+  if (typeof window.webln === "undefined") {
+    return alert("No WebLN available.");
+  }
+
+  try {
+    await window.webln.enable();
+    const result = await window.webln.makeInvoice({
+      amount: 21
+    });
+    showResult(result);    
+    celebrate();
+  } catch (error) {
+    console.log(error);
+    alert("An error occurred during the makeInvoice() call.");
+  }
+}
+
+async function sendPayment() {
+  if (typeof window.webln === "undefined") {
+    return alert("No WebLN available.");
+  }
+
+  try {
+    await window.webln.enable();
+    const invoice = prompt("Please provide an invoice (lnbc...)");
+    const result = await window.webln.sendPayment(invoice);
+    showResult(result);
+    celebrate();
+  } catch (error) {
+    alert("An error occurred during the payment.");
+  }
+}
+
